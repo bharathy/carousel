5d88d25b576100eaf9530894822e779cc1f93aec
Bharathy Seralathan <bharathy.seralathan@macbook-pro-6.home>
2019-02-28 09:05:11 +0000

    [carousel] - Added one unit test

--- test/components/carousel.spec.jsx
+++ test/components/carousel.spec.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 
-// import carousel from '../../src/components/carousel';
+import Carousel from '../../src/components/carousel';
 
-// const testProps = {
-// 	images: ['img']
-// }
+const sandbox = sinon.createSandbox();
+const images = [{
+	PreviewUrl: 'slide.png',
+	user: 'test user',
+	likes: 20}];
+const testProps = {
+	images: images,
+	fetchImages: sandbox.stub(),
+	store: { getState: sandbox.stub().returns(images),
+	subscribe: sandbox.spy() }
+}
 
-// describe('Given a carousel component', () => {
-// 	describe('When component is rendered', () => {
-// 		let wrapper;
+describe('Given a carousel component', () => {
+	describe('When component is rendered', () => {
+		let wrapper;
 
-// 		beforeEach(() => wrapper = shallow(<Carousel { ...testProps} />));
+		beforeEach(() => wrapper = shallow(<Carousel { ...testProps} />));
 
-// 		it('should render the carosusel', () => {
-// 			expect(wrapper).to.have.length(1);
-// 		});
-// 	});
-// });
+		it('should render the carosusel', () => {
+			expect(wrapper).to.have.length(1);
+		});
+	});
+});
